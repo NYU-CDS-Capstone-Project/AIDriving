1008eba4f19b51a2955e478d88c179633c49ded3
raviatnyu <raviatnyu@github.com>
2018-11-08 09:32:26 -0500

    Removing pytorch 0.4 dependency

--- pytorch_rl/main.py
+++ pytorch_rl/main.py
@@ -196,7 +196,7 @@ def main():
             (value_loss * args.value_loss_coef + action_loss - dist_entropy * args.entropy_coef).backward()
 
             if args.algo == 'a2c':
-                nn.utils.clip_grad_norm_(actor_critic.parameters(), args.max_grad_norm)
+                nn.utils.clip_grad_norm(actor_critic.parameters(), args.max_grad_norm)
 
             optimizer.step()
 
@@ -267,25 +267,11 @@ def main():
                     total_num_steps,
                     int(total_num_steps / (end - start)),
                     reward_avg,
-                    dist_entropy.item(),
-                    value_loss.item(),
-                    action_loss.item()
+                    dist_entropy.data[0],
+                    value_loss.data[0],
+                    action_loss.data[0]
                 )
             )
-
-            """
-            print("Updates {}, num timesteps {}, FPS {}, mean/median reward {:.1f}/{:.1f}, min/max reward {:.1f}/{:.1f}, entropy {:.5f}, value loss {:.5f}, policy loss {:.5f}".
-                format(
-                    j,
-                    total_num_steps,
-                    int(total_num_steps / (end - start)),
-                    final_rewards.mean(),
-                    final_rewards.median(),
-                    final_rewards.min(),
-                    final_rewards.max(), dist_entropy.data[0],
-                    value_loss.data[0], action_loss.data[0])
-                )
-            """
 
         if args.vis and j % args.vis_interval == 0:
             try:
