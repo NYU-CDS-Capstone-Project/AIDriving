06b02f9f718530e41aec4b7d1190c95724ff6847
raviatnyu <raviatnyu@github.com>
2018-11-08 10:06:25 -0500

    Making the continous action space as default

--- pytorch_rl/envs.py
+++ pytorch_rl/envs.py
@@ -10,10 +10,11 @@ import gym_duckietown
 from gym_duckietown.envs import *
 from gym_duckietown.wrappers import *
 
-def make_env(env_id, seed, rank, log_dir, start_container):
+def make_env(env_id, seed, rank, log_dir, start_container, discrete_wrapper=False):
     def _thunk():
         env = gym.make(env_id)
-        env = DiscreteWrapper(env)
+        if discrete_wrapper:
+            env = DiscreteWrapper(env)
 
         env.seed(seed + rank)
 
